2d6022ba4c323e21679901a62fda648e16be647c
ONCGM <oncraftgames@gmail.com>
2019-10-16 17:21:17 -0300

    Initial project commit.
    Second backup of the project, Master is on Unity Collab.

---
+++ Gear VR/Assets/Scripts/VREnums/Enums.cs
@@ -0,0 +1,17 @@
+﻿namespace VREnums {
+    /// <summary>
+    /// Defines the type of leaning input to be used.
+    /// </summary>
+    public enum InputDirection {
+        Forward,
+        Left,
+        Right,
+        Backward,
+        ForwardLeft,
+        ForwardRight,
+        BackwardLeft,
+        BackwardRight,
+        Centered,
+        InvalidDirection
+    }
+}

839f3a035e9bfe11b44761e586a35338a3f5fac9
ONCGM <ONCGM@noreply.github.com>
2020-08-11 17:05:13 -0300

    ID and Playing position update

---
+++ Assets/Scripts/ONCGM/Utility/SaveData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using ONCGM.Game;
+
+namespace ONCGM.Utility {
+    /// <summary>
+    /// Class to hold game data and settings data.
+    /// </summary>
+    [Serializable]
+    public class SaveData {
+        // Save data.
+        /// <summary>
+        /// The player name.
+        /// </summary>
+        public string PlayerName = "Jogador";
+
+        /// <summary>
+        /// The age of the player.
+        /// </summary>
+        public int PlayerAge = 0;
+
+        /// <summary>
+        /// The patient medical id.
+        /// </summary>
+        public string PatientId = string.Empty;
+        
+        /// <summary>
+        /// Did the player stand for this session.
+        /// </summary>
+        public bool isStanding = false;
+
+        // Stats data.
+        /// <summary>
+        /// How many times the player calibrated his device.
+        /// </summary>
+        public int TimesCalibrated = 0;
+        
+        /// <summary>
+        /// The game session that this player generated during play time.
+        /// </summary>
+        public List<GameSession> PlayerSessions = new List<GameSession>();
+        
+        // Settings.
+        /// <summary>
+        /// Should the game music be enabled.
+        /// </summary>
+        public bool musicEnabled = true;
+
+        /// <summary>
+        /// Should the game sfx be enabled.
+        /// </summary>
+        public bool sfxEnabled = true;
+    }
+}
---
+++ Assets/Scripts/ONCGM/VR/VRButtons/ToggleSeatedPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ONCGM.Utility;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ONCGM.VR.VRButtons {
+    /// <summary>
+    /// A toggle button based on a TMP_Text button and using DoTween for animations.
+    /// Toggles seated position in serialization.
+    /// Does not influence anything in gameplay.
+    /// </summary>
+    public class ToggleSeatedPosition : ToggleButton {
+        /// <summary>
+        /// Override pointer click to change the position setting.
+        /// </summary>
+        public override void OnPointerClick(PointerEventData eventData) {
+            if(!canClick) return;
+            onClickHandler.Invoke();
+            ToggleState = !ToggleState;
+            canClick = false;
+            StartCoroutine(nameof(UnlockClick));
+            SaveSystem.LoadedData.isStanding = ToggleState;
+            SaveSystem.SaveGameToFile();
+        }
+    }
+}
